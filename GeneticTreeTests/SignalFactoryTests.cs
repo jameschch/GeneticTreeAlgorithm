52c822b8413f439c886610ba740dbb4a85eb8d04
jameschch@outlook.com <jameschch@outlook.com>
2017-11-23 15:23:48 +0000

    Adds test coverage of indicator instantiation

--- GeneticTreeTests/SignalFactoryTests.cs
+++ GeneticTreeTests/SignalFactoryTests.cs
@@ -10,6 +10,10 @@ using GeneticTree.Signal;
 using QuantConnect;
 using QuantConnect.Algorithm;
 using System.Runtime.Serialization;
+using QuantConnect.Data;
+using System.Dynamic;
+using NodaTime;
+using QuantConnect.Indicators;
 
 namespace GeneticTree.Tests
 {
@@ -25,6 +29,13 @@ namespace GeneticTree.Tests
 
             var fakeSymbol = Symbol.Create("ABCXYZ", SecurityType.Equity, Market.USA);
 
+            fakeAlgorithm.SubscriptionManager = new SubscriptionManagerWrapper(fakeSymbol, null, null);
+
+            //todo: remaining indicators
+            var expectedEntry = new[] { "None", "SimpleMovingAverage", "MovingAverageConvergenceDivergence", "Stochastic", "RelativeStrengthIndex" };
+
+            var expectedExit = new[] {  "CommodityChannelIndex", "MomentumPercent", "WilliamsPercentR", "PercentagePriceOscillator", "AverageDirectionalIndex" };
+
             var actual = unit.Create(fakeAlgorithm, fakeSymbol, true);
 
             Assert.AreEqual(Operator.And, actual.List.First().Operator);
@@ -32,12 +43,17 @@ namespace GeneticTree.Tests
             Assert.AreEqual(Operator.OrInclusive, actual.List.First().Child.Child.Operator);
             Assert.AreEqual(Operator.Not, actual.List.First().Child.Child.Child.Operator);
 
+            CollectionAssert.AreEquivalent(expectedEntry, actual.List.Select(l => l.Name));
+
             actual = unit.Create(fakeAlgorithm, fakeSymbol, false);
 
             Assert.AreEqual(Operator.Nor, actual.List.First().Operator);
             Assert.AreEqual(Operator.NorInclusive, actual.List.First().Child.Operator);
             Assert.AreEqual(Operator.And, actual.List.First().Child.Child.Operator);
             Assert.AreEqual(Operator.Or, actual.List.First().Child.Child.Child.Operator);
+
+            CollectionAssert.AreEquivalent(expectedExit, actual.List.Select(l => l.Name));
+
         }
 
         protected class SignalFactoryWrapper : SignalFactory
@@ -58,11 +74,11 @@ namespace GeneticTree.Tests
                 {"EntryOperator2",  1},
                 {"EntryOperator3",  2},
                 {"EntryOperator4",  3},
-                {"ExitIndicator1",  6},
+                {"ExitIndicator1",  4},
                 {"ExitIndicator2",  5},
-                {"ExitIndicator3",  4},
-                {"ExitIndicator4",  -1},
-                {"ExitIndicator5",  2},
+                {"ExitIndicator3",  6},
+                {"ExitIndicator4",  7},
+                {"ExitIndicator5",  8},
                 {"ExitIndicator1Direction",  0},
                 {"ExitIndicator2Direction",  0},
                 {"ExitIndicator3Direction",  1},
@@ -78,15 +94,23 @@ namespace GeneticTree.Tests
                 {"signalPeriod",  4 }
             };
 
-            protected override ISignal CreateIndicator(Symbol pair, int i, string entryOrExit)
-            {
-                return new EmptySignal();
-            }
-
             protected override int GetConfigValue(string key)
             {
                 return config[key];
             }
+
+        }
+
+        protected class SubscriptionManagerWrapper : SubscriptionManager
+        {
+            public SubscriptionManagerWrapper(Symbol symbol, AlgorithmSettings algorithmSettings, TimeKeeper timeKeeper) : base(algorithmSettings, timeKeeper)
+            {
+
+                this.Subscriptions = new HashSet<SubscriptionDataConfig> { new SubscriptionDataConfig(typeof(BaseData), symbol, Resolution.Hour, DateTimeZone.Utc,
+                    DateTimeZone.Utc, false, false, false, false, TickType.Quote) };
+            }
+
+
 
         }
 
